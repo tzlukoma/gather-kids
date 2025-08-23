7fea4ab102a7e45082a85fbd995334336a6daee8
Thomas Z Lukoma <anon@example.com>
2025-08-23 05:12:42 +0000

    No, the thank you message is supposed to be in the same alert as the res

--- src/components/ministrysync/dance-ministry-form.tsx
+++ src/components/ministrysync/dance-ministry-form.tsx
@@ -14,39 +14,41 @@ import { Alert, AlertDescription } from "../ui/alert"
 export function DanceMinistryForm({ control }: { control: any }) {
     return (
         <div className="mt-4 space-y-4">
-            <FormField
-                control={control}
-                name="customData.dance_returning_member"
-                render={({ field }) => (
-                <FormItem className="space-y-3">
-                    <FormLabel>Are you a returning member of the dance ministry?</FormLabel>
-                    <FormControl>
-                    <RadioGroup
-                        onValueChange={field.onChange}
-                        defaultValue={field.value}
-                        className="flex flex-col space-y-1"
-                    >
-                        <FormItem className="flex items-center space-x-3 space-y-0">
-                        <FormControl>
-                            <RadioGroupItem value="yes" />
-                        </FormControl>
-                        <FormLabel className="font-normal">Yes</FormLabel>
-                        </FormItem>
-                        <FormItem className="flex items-center space-x-3 space-y-0">
-                        <FormControl>
-                            <RadioGroupItem value="no" />
-                        </FormControl>
-                        <FormLabel className="font-normal">No</FormLabel>
-                        </FormItem>
-                    </RadioGroup>
-                    </FormControl>
-                    <FormMessage />
-                </FormItem>
-                )}
-            />
-             <Alert>
+            <Alert>
                 <AlertDescription>
-                    Thank you for registering for the Dance Ministry. You will receive information from ministry leaders regarding next steps for your child's participation.
+                    <div className="space-y-4">
+                        <FormField
+                            control={control}
+                            name="customData.dance_returning_member"
+                            render={({ field }) => (
+                            <FormItem className="space-y-3">
+                                <FormLabel>Are you a returning member of the dance ministry?</FormLabel>
+                                <FormControl>
+                                <RadioGroup
+                                    onValuecha  nge={field.onChange}
+                                    defaultValue={field.value}
+                                    className="flex flex-col space-y-1"
+                                >
+                                    <FormItem className="flex items-center space-x-3 space-y-0">
+                                    <FormControl>
+                                        <RadioGroupItem value="yes" />
+                                    </FormControl>
+                                    <FormLabel className="font-normal">Yes</FormLabel>
+                                    </FormItem>
+                                    <FormItem className="flex items-center space-x-3 space-y-0">
+                                    <FormControl>
+                                        <RadioGroupItem value="no" />
+                                    </FormControl>
+                                    <FormLabel className="font-normal">No</FormLabel>
+                                    </FormItem>
+                                </RadioGroup>
+                                </FormControl>
+                                <FormMessage />
+                            </FormItem>
+                            )}
+                        />
+                        <p>Thank you for registering for the Dance Ministry. You will receive information from ministry leaders regarding next steps for your child's participation.</p>
+                    </div>
                 </AlertDescription>
             </Alert>
         </div>
