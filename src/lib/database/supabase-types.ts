4ef9eae93f067a0e70c55ee6d5cb01e34c40aa45
GitHub Action <action@github.com>
2025-09-11 05:37:01 +0000

    [auto] Update Supabase type definitions

--- src/lib/database/supabase-types.ts
+++ src/lib/database/supabase-types.ts
@@ -1,7 +1,7 @@
 /**
  * This file contains types generated from the Supabase schema.
  * DO NOT EDIT MANUALLY. This file is auto-generated.
- * Generated on: 2025-09-11T05:34:26.587Z
+ * Generated on: 2025-09-11T05:37:00.896Z
  */
 
 export type SupabaseJson =
