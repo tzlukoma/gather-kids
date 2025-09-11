7788ea64c10ba2317fe7a93dffbac87c539cd816
Thomas Z Lukoma <3255492+tzlukoma@users.noreply.github.com>
2025-09-11 00:47:17 -0400

    chore: update auto-generated Supabase types file timestamp

--- src/lib/database/supabase-types.ts
+++ src/lib/database/supabase-types.ts
@@ -1,7 +1,7 @@
 /**
  * This file contains types generated from the Supabase schema.
  * DO NOT EDIT MANUALLY. This file is auto-generated.
- * Generated on: 2025-09-11T04:44:25.329Z
+ * Generated on: 2025-09-11T04:46:56.790Z
  */
 
 export type SupabaseJson =
